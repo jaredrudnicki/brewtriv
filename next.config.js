7f6cb4d8eb67521166fbff123cf6252b0b74e4e4
Jared Rudnicki <rudnicki.j@northeastern.edu>
2024-06-29 14:05:24 -0400

    config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
-    basePath: '/',
     env: {
         key: 'rudnickey',
         url: isProd ? "https://www.brewtriv.com" : "http://localhost:3000"
